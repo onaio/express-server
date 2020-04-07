a905fd15bb6bcad6b86d01f7bae78266f3d466c4
p-netm <pmuriuki98@gmail.com>
2020-04-07 14:26:53 +0300

    Replace server with app in tests
    Work around for dealing due to error: Jest has detected the following 1 open handle potentially keeping Jest from exiting:
    
      ●  TCPSERVERWRAP

--- src/app/tests/index.test.ts
+++ src/app/tests/index.test.ts
@@ -3,7 +3,7 @@ import ClientOauth2 from 'client-oauth2';
 import nock from 'nock';
 import request from 'supertest';
 import { EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL, EXPRESS_SESSION_LOGIN_URL } from '../../configs/envs';
-import server from '../../index';
+import app from '../index';
 import { oauthState, parsedApiResponse, unauthorized } from './fixtures';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -12,13 +12,13 @@ const { extractCookies } = require('./utils');
 const authorizationUri = 'http://reveal-stage.smartregister.org/opensrp/oauth/';
 const oauthCallbackUri = '/oauth/callback/OpenSRP/?code=Boi4Wz&state=opensrp';
 
-const panic = (err: Error, done: jest.DoneCallback) => {
+const panic = (err: Error, done: jest.DoneCallback): void => {
     if (err) {
         done(err);
     }
 };
 
-jest.mock('../configs/envs');
+jest.mock('../../configs/envs');
 
 jest.mock('client-oauth2', () => {
     class CodeFlow {
@@ -84,108 +84,100 @@ describe('src/index.ts', () => {
         jest.resetAllMocks();
     });
 
-    afterAll((done) => {
-        server.close(done);
+    it('serves the build.index.html file', (done) => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .expect('Do you mind')
+            .end((err: Error) => {
+                if (err) {
+                    throw err;
+                }
+                done();
+            });
     });
 
-    it('adds correctly', () => {
-        expect(2).toEqual(2);
+    it('oauth/opensrp redirects to auth-server', (done) => {
+        request(app)
+            .get('/oauth/opensrp')
+            .expect(302)
+            .end((err: Error, res) => {
+                panic(err, done);
+                expect(res.header.location).toEqual(authorizationUri);
+                expect(res.notFound).toBeFalsy();
+                expect(res.redirect).toBeTruthy();
+                done();
+            });
     });
 
-    // it('serves the build.index.html file', (done) => {
-    //     request(server)
-    //         .get('/')
-    //         .expect(200)
-    //         .expect('Do you mind')
-    //         .end((err: Error, _: request.Response) => {
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             done();
-    //         });
-    // });
+    it('E2E: oauth/opensrp/callback works correctly', async (done) => {
+        JSON.parse = (body) => {
+            if (body === '{}') {
+                return parsedApiResponse;
+            }
+        };
+        nock('http://reveal-stage.smartregister.org').get(`/opensrp/user-details`).reply(200, {});
 
-    // it('oauth/opensrp redirects to auth-server', (done) => {
-    //     request(server)
-    //         .get('/oauth/opensrp')
-    //         .expect(302)
-    //         .end((err: Error, res) => {
-    //             panic(err, done);
-    //             expect(res.header.location).toEqual(authorizationUri);
-    //             expect(res.notFound).toBeFalsy();
-    //             expect(res.redirect).toBeTruthy();
-    //             done();
-    //         });
-    // });
+        /** PS: This test will start failing on  Fri, 14 May 3019 11:55:39 GMT */
+        jest.spyOn(global.Date, 'now').mockImplementationOnce(() => new Date('3019-05-14T11:01:58.135Z').valueOf());
 
-    // it('E2E: oauth/opensrp/callback works correctly', async (done) => {
-    //     JSON.parse = (body) => {
-    //         if (body === '{}') {
-    //             return parsedApiResponse;
-    //         }
-    //     };
-    //     nock('http://reveal-stage.smartregister.org').get(`/opensrp/user-details`).reply(200, {});
+        request(app)
+            .get(oauthCallbackUri)
+            .end((err, res: request.Response) => {
+                panic(err, done);
+                expect(res.header.location).toEqual(EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL);
+                expect(res.notFound).toBeFalsy();
+                expect(res.redirect).toBeTruthy();
+                sessionString = res.header['set-cookie'][0].split(';')[0];
+                cookie = extractCookies(res.header);
+                // expect that cookie will expire in: now(a date mocked to be in the future) + token.expires_in
+                expect(cookie['reveal-session'].flags).toEqual({
+                    Expires: 'Fri, 14 May 3019 11:55:39 GMT',
+                    HttpOnly: true,
+                    Path: '/',
+                });
+                done();
+            });
+    });
 
-    //     /** PS: This test will start failing on  Fri, 14 May 3019 11:55:39 GMT */
-    //     jest.spyOn(global.Date, 'now').mockImplementationOnce(() => new Date('3019-05-14T11:01:58.135Z').valueOf());
+    it('/oauth/state works correctly without cookie', (done) => {
+        request(app)
+            .get('/oauth/state')
+            .end((err, res) => {
+                panic(err, done);
+                expect(res.body).toEqual(unauthorized);
+                done();
+            });
+    });
 
-    //     request(server)
-    //         .get(oauthCallbackUri)
-    //         .end((err, res: request.Response) => {
-    //             panic(err, done);
-    //             expect(res.header.location).toEqual(EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL);
-    //             expect(res.notFound).toBeFalsy();
-    //             expect(res.redirect).toBeTruthy();
-    //             sessionString = res.header['set-cookie'][0].split(';')[0];
-    //             cookie = extractCookies(res.header);
-    //             // expect that cookie will expire in: now(a date mocked to be in the future) + token.expires_in
-    //             expect(cookie['reveal-session'].flags).toEqual({
-    //                 Expires: 'Fri, 14 May 3019 11:55:39 GMT',
-    //                 HttpOnly: true,
-    //                 Path: '/',
-    //             });
-    //             done();
-    //         });
-    // });
+    it('/oauth/state works correctly with cookie', (done) => {
+        request(app)
+            .get('/oauth/state')
+            .set('cookie', sessionString)
+            // .send()
+            .end((err: Error, res: request.Response) => {
+                panic(err, done);
+                expect(res.body).toEqual(oauthState);
+                done();
+            });
+    });
 
-    // it('/oauth/state works correctly without cookie', (done) => {
-    //     request(server)
-    //         .get('/oauth/state')
-    //         .end((err, res) => {
-    //             panic(err, done);
-    //             expect(res.body).toEqual(unauthorized);
-    //             done();
-    //         });
-    // });
-
-    // it('/oauth/state works correctly with cookie', (done) => {
-    //     request(server)
-    //         .get('/oauth/state')
-    //         .set('cookie', sessionString)
-    //         // .send()
-    //         .end((err: Error, res: request.Response) => {
-    //             panic(err, done);
-    //             expect(res.body).toEqual(oauthState);
-    //             done();
-    //         });
-    // });
-
-    // it('logs user out with cookie', (done) => {
-    //     request(server)
-    //         .get('/logout')
-    //         .set('Cookie', sessionString)
-    //         .end((err, res: request.Response) => {
-    //             panic(err, done);
-    //             expect(res.header.location).toEqual(EXPRESS_SESSION_LOGIN_URL);
-    //             expect(res.redirect).toBeTruthy();
-    //             // check that session is revoked
-    //             request(server)
-    //                 .get('/oauth/state')
-    //                 .end((e: Error, r: request.Response) => {
-    //                     panic(e, done);
-    //                     expect(r.body).toEqual(unauthorized);
-    //                     done();
-    //                 });
-    //         });
-    // });
+    it('logs user out with cookie', (done) => {
+        request(app)
+            .get('/logout')
+            .set('Cookie', sessionString)
+            .end((err, res: request.Response) => {
+                panic(err, done);
+                expect(res.header.location).toEqual(EXPRESS_SESSION_LOGIN_URL);
+                expect(res.redirect).toBeTruthy();
+                // check that session is revoked
+                request(app)
+                    .get('/oauth/state')
+                    .end((e: Error, r: request.Response) => {
+                        panic(e, done);
+                        expect(r.body).toEqual(unauthorized);
+                        done();
+                    });
+            });
+    });
 });
