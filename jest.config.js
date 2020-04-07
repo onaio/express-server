6ece5d5ebf1986ae1046b270ab48662c9c5778e8
p-netm <pmuriuki98@gmail.com>
2020-04-07 15:00:27 +0300

    Refacor script to run tests with coverage
    Running tests with coverage without --no-cache results in an error that jest was not able to find some source files. https://github.com/kulshekhar/ts-jest/issues/60.

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,18 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  coveragePathIgnorePatterns: ["src/tests"],
-  coverageReporters: ["lcov", "html"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 85,
-      lines: 85,
-      statements: 90,
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
     },
-  },
+    testEnvironment: 'node',
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/tests/', '!src/index.ts'],
+    coverageReporters: ['lcov', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 85,
+            lines: 85,
+            statements: 90,
+        },
+    },
 };
