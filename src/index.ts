5950ede0863a248d4d365dbfd6acefc0f6e302c2
Macharia Muguku <machariamuguku@gmail.com>
2021-10-18 11:39:35 +0300

    pass start notification log to winston

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import app from './app';
 import { EXPRESS_PORT } from './configs/envs';
+import { winstonLogger } from './configs/winston';
 
 const PORT = EXPRESS_PORT || 3000;
 const server = app.listen(PORT, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`App listening on port ${PORT}!`);
+    // log every time app starts
+    winstonLogger.info(`App listening on port ${PORT}!`);
 });
 
 export default server;
