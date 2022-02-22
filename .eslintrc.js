39276f5849d3097ea4f41fd3ae1f72fafe7c76be
Macharia Muguku <machariamuguku@gmail.com>
2022-02-22 05:19:16 +0300

    reduce tab width to 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,63 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'airbnb-base',
-        'airbnb-typescript/base',
-        'plugin:prettier/recommended',
-        'typestrict',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+    'typestrict',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // variableLike - matches the same as variable, function and parameter
+      {
+        selector: 'variableLike',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+      // typeLike - matches the same as class, interface, typeAlias, enum, typeParameter
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
     ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+    //ignore extensions for importing files with .js and .ts extensions
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+        moduleDirectory: ['src', 'node_modules'],
+      },
+      project: './tsconfig.json',
     },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-        'prettier/prettier': 2,
-        '@typescript-eslint/naming-convention': [
-            'error',
-            // variableLike - matches the same as variable, function and parameter
-            {
-                selector: 'variableLike',
-                format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-                leadingUnderscore: 'allow',
-            },
-            // typeLike - matches the same as class, interface, typeAlias, enum, typeParameter
-            {
-                selector: 'typeLike',
-                format: ['PascalCase'],
-            },
-        ],
-        //ignore extensions for importing files with .js and .ts extensions
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                js: 'never',
-                ts: 'never',
-            },
-        ],
-    },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.ts'],
-                moduleDirectory: ['src', 'node_modules'],
-            },
-            project: './tsconfig.json',
-        },
-    },
+  },
 };
