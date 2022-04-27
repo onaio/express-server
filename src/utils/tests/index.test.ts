e8f341de4b9c4624a3b59cbf07722e17d153423a
Peter Muriuki <pmuriuki98@gmail.com>
2022-04-27 15:50:03 +0300

    Revert "test unhappy path"
    This reverts commit 2f33ca8d81b582c00805b5aa3ccbdf4aeff07acd.

--- src/utils/tests/index.test.ts
+++ src/utils/tests/index.test.ts
@@ -6,11 +6,4 @@ describe('test util functions', () => {
     const result = getOriginFromUrl(url);
     expect(result).toEqual(['https://keycloak-example.domain.org']);
   });
-  it('returns empty array if url is empty or undefined', () => {
-    const emptyUrl = '';
-    const emptyUrlResult = getOriginFromUrl(emptyUrl);
-    const undefinedUrlResult = getOriginFromUrl();
-    expect(emptyUrlResult).toEqual([]);
-    expect(undefinedUrlResult).toEqual([]);
-  });
 });
