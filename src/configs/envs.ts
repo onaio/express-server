1b770a18043456d22a35a9dfcd3238849944e99c
fMurugi <81636349+fMurugi@users.noreply.github.com>
2022-08-16 20:58:56 +0300

    Removed explicit types exports from config/envs.ts (#35)
    Co-authored-by: fMurugi <fionaMurugi99@gamil.com>

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -13,91 +13,65 @@ dotenv.config();
  */
 export const EXPRESS_OPENSRP_ACCESS_TOKEN_URL =
   process.env.EXPRESS_OPENSRP_ACCESS_TOKEN_URL || 'https://opensrp-stage.smartregister.org/opensrp/oauth/token';
-export type EXPRESS_OPENSRP_ACCESS_TOKEN_URL = typeof EXPRESS_OPENSRP_ACCESS_TOKEN_URL;
 
 export const EXPRESS_OPENSRP_AUTHORIZATION_URL =
   process.env.EXPRESS_OPENSRP_AUTHORIZATION_URL || 'https://opensrp-stage.smartregister.org/opensrp/oauth/authorize';
-export type EXPRESS_OPENSRP_AUTHORIZATION_URL = typeof EXPRESS_OPENSRP_AUTHORIZATION_URL;
 
 export const EXPRESS_OPENSRP_CALLBACK_URL =
   process.env.EXPRESS_OPENSRP_CALLBACK_URL || 'http://localhost:3000/oauth/callback/OpenSRP/';
-export type EXPRESS_OPENSRP_CALLBACK_URL = typeof EXPRESS_OPENSRP_CALLBACK_URL;
 
 export const EXPRESS_OPENSRP_USER_URL =
   process.env.EXPRESS_OPENSRP_USER_URL || 'https://opensrp-stage.smartregister.org/opensrp/user-details';
-export type EXPRESS_OPENSRP_USER_URL = typeof EXPRESS_OPENSRP_USER_URL;
 
 export const EXPRESS_SESSION_FILESTORE_PATH = process.env.EXPRESS_SESSION_FILESTORE_PATH || '/tmp/express-sessions';
-export type EXPRESS_SESSION_FILESTORE_PATH = typeof EXPRESS_SESSION_FILESTORE_PATH;
 
 export const EXPRESS_PRELOADED_STATE_FILE = process.env.EXPRESS_PRELOADED_STATE_FILE || '/tmp/expressState.json';
-export type EXPRESS_PRELOADED_STATE_FILE = typeof EXPRESS_PRELOADED_STATE_FILE;
 
 export const EXPRESS_SESSION_LOGIN_URL = process.env.EXPRESS_SESSION_LOGIN_URL || '/login';
-export type EXPRESS_SESSION_LOGIN_URL = typeof EXPRESS_SESSION_LOGIN_URL;
 
 export const EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL =
   process.env.EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL || '/fe/oauth/callback/opensrp';
-export type EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL = typeof EXPRESS_FRONTEND_OPENSRP_CALLBACK_URL;
 
 export const EXPRESS_OPENSRP_OAUTH_STATE = process.env.EXPRESS_OPENSRP_OAUTH_STATE || 'opensrp';
-export type EXPRESS_OPENSRP_OAUTH_STATE = typeof EXPRESS_OPENSRP_OAUTH_STATE;
 
 export const { EXPRESS_OPENSRP_CLIENT_ID } = process.env;
-export type EXPRESS_OPENSRP_CLIENT_ID = typeof EXPRESS_OPENSRP_CLIENT_ID;
 
 export const { EXPRESS_OPENSRP_CLIENT_SECRET } = process.env;
-export type EXPRESS_OPENSRP_CLIENT_SECRET = typeof EXPRESS_OPENSRP_CLIENT_SECRET;
 
 export const EXPRESS_PORT = parseInt(process.env.EXPRESS_PORT || '3000', 10);
-export type EXPRESS_PORT = typeof EXPRESS_PORT;
 
 export const EXPRESS_SESSION_NAME = process.env.EXPRESS_SESSION_NAME || 'express-session';
-export type EXPRESS_SESSION_NAME = typeof EXPRESS_SESSION_NAME;
 
 export const EXPRESS_SESSION_SECRET = process.env.EXPRESS_SESSION_SECRET || 'hunter2';
-export type EXPRESS_SESSION_SECRET = typeof EXPRESS_SESSION_SECRET;
 
 export const EXPRESS_SESSION_PATH = process.env.EXPRESS_SESSION_PATH || '/';
-export type EXPRESS_SESSION_PATH = typeof EXPRESS_SESSION_PATH;
 
 export const EXPRESS_REACT_BUILD_PATH =
   process.env.EXPRESS_REACT_BUILD_PATH || path.resolve(path.resolve(), '../build');
-export type EXPRESS_REACT_BUILD_PATH = typeof EXPRESS_REACT_BUILD_PATH;
 
 export const EXPRESS_FRONTEND_LOGIN_URL = process.env.EXPRESS_FRONTEND_LOGIN_URL || '/fe/login';
-export type EXPRESS_FRONTEND_LOGIN_URL = typeof EXPRESS_FRONTEND_LOGIN_URL;
 
 export const EXPRESS_ALLOW_TOKEN_RENEWAL = process.env.EXPRESS_ALLOW_TOKEN_RENEWAL === 'true';
-export type EXPRESS_ALLOW_TOKEN_RENEWAL = typeof EXPRESS_ALLOW_TOKEN_RENEWAL;
 
 export const EXPRESS_MAXIMUM_SESSION_LIFE_TIME = Number(process.env.EXPRESS_MAXIMUM_SESSION_LIFE_TIME || 3 * 60 * 60); // 3hrs default
-export type EXPRESS_MAXIMUM_SESSION_LIFE_TIME = typeof EXPRESS_MAXIMUM_SESSION_LIFE_TIME;
 
 export const EXPRESS_SERVER_LOGOUT_URL = process.env.EXPRESS_SERVER_LOGOUT_URL || 'http://localhost:3000/logout';
-export type EXPRESS_SERVER_LOGOUT_URL = typeof EXPRESS_SERVER_LOGOUT_URL;
 
 export const EXPRESS_OPENSRP_LOGOUT_URL =
   process.env.EXPRESS_OPENSRP_LOGOUT_URL || 'https://opensrp-stage.smartregister.org/opensrp/logout.do';
-export type EXPRESS_OPENSRP_LOGOUT_URL = typeof EXPRESS_OPENSRP_LOGOUT_URL;
 
 export const EXPRESS_KEYCLOAK_LOGOUT_URL =
   process.env.EXPRESS_KEYCLOAK_LOGOUT_URL ||
   'https://keycloak-stage.smartregister.org/auth/realms/opensrp-web-stage/protocol/openid-connect/logout';
-export type EXPRESS_KEYCLOAK_LOGOUT_URL = typeof EXPRESS_KEYCLOAK_LOGOUT_URL;
 
 export const EXPRESS_MAXIMUM_LOGS_FILE_SIZE = Number(process.env.EXPRESS_MAXIMUM_LOGS_FILE_SIZE || 5242880); // 5MB
-export type EXPRESS_MAXIMUM_LOGS_FILE_SIZE = typeof EXPRESS_MAXIMUM_LOGS_FILE_SIZE;
 
 export const EXPRESS_MAXIMUM_LOG_FILES_NUMBER = Number(process.env.EXPRESS_MAXIMUM_LOG_FILES_NUMBER || 5);
-export type EXPRESS_MAXIMUM_LOG_FILES_NUMBER = typeof EXPRESS_MAXIMUM_LOG_FILES_NUMBER;
 
 export const EXPRESS_LOGS_FILE_PATH = process.env.EXPRESS_LOGS_FILE_PATH || './logs/default-error.log';
-export type EXPRESS_LOGS_FILE_PATH = typeof EXPRESS_LOGS_FILE_PATH;
 
 export const EXPRESS_COMBINED_LOGS_FILE_PATH =
   process.env.EXPRESS_COMBINED_LOGS_FILE_PATH || './logs/default-error-and-info.log';
-export type EXPRESS_COMBINED_LOGS_FILE_PATH = typeof EXPRESS_COMBINED_LOGS_FILE_PATH;
 
 const defaultCsp = JSON.stringify({
   'default-src': ['none'],
@@ -106,7 +80,6 @@ const defaultCsp = JSON.stringify({
 export const EXPRESS_CONTENT_SECURITY_POLICY_CONFIG = JSON.parse(
   process.env.EXPRESS_CONTENT_SECURITY_POLICY_CONFIG || defaultCsp,
 );
-export type EXPRESS_CONTENT_SECURITY_POLICY_CONFIG = typeof EXPRESS_CONTENT_SECURITY_POLICY_CONFIG;
 
 // see https://github.com/luin/ioredis#connect-to-redis
 export const { EXPRESS_REDIS_STAND_ALONE_URL } = process.env;
@@ -115,4 +88,3 @@ export const { EXPRESS_REDIS_STAND_ALONE_URL } = process.env;
 export const EXPRESS_REDIS_SENTINEL_CONFIG = JSON.parse(process.env.EXPRESS_REDIS_SENTINEL_CONFIG || '{}');
 
 export const EXPRESS_RESPONSE_HEADERS = JSON.parse(process.env.EXPRESS_RESPONSE_HEADERS || '{}');
-export type EXPRESS_RESPONSE_HEADERS = typeof EXPRESS_RESPONSE_HEADERS;
