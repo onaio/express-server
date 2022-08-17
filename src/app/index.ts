e6c295e74f5838c9d68d8475e1f62c60d69e3883
Peter Muriuki <pmuriuki98@gmail.com>
2022-08-17 12:29:39 +0300

    Add profile to scopes

--- src/app/index.ts
+++ src/app/index.ts
@@ -48,7 +48,7 @@ const opensrpAuth = new ClientOAuth2({
   clientId: EXPRESS_OPENSRP_CLIENT_ID,
   clientSecret: EXPRESS_OPENSRP_CLIENT_SECRET,
   redirectUri: EXPRESS_OPENSRP_CALLBACK_URL,
-  scopes: ['openid'],
+  scopes: ['openid', 'profile'],
   state: EXPRESS_OPENSRP_OAUTH_STATE,
 });
 const loginURL = EXPRESS_SESSION_LOGIN_URL;
