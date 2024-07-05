c1eefee31a78cd8ece56b0be0ffaec2b891933c1
Peter Muriuki <pmuriuki98@gmail.com>
2024-07-05 12:17:56 +0300

    Delete dist folder

--- build/dist/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable @typescript-eslint/no-empty-interface */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-require("express-session");
-require("redis");
-//# sourceMappingURL=index.js.map
